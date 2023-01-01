252314782279a44208267a6304076a6ff1d3d892
Dylan Pruitt <dpruitt0703@gmail.com>
2023-01-01 18:29:42 -0500

    Added Github/LinkedIn icons to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,24 @@ import { Link } from "react-router-dom";
 import './Home.css';
 
 const Home = () => {
-    
+
     return (
         <div>
             <h1>React Tactics</h1>
-            <p>This page does exist!</p>
-            <Link to="/LevelSelect" className="link"> Level Select </Link>
+            <p>Tactics game created for web browsers.</p>
+            <Link to="/LevelSelect" className="link">
+                <button>
+                    Level Select
+                </button>
+            </Link>
             <section className="bottom-container">
                 <p>by Dylan Pruitt</p>
+                <a href="https://github.com/dylanpruitt/react-tactics-game" target="_blank" rel="noreferrer">
+                    <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/dylan-pruitt-595304228/" target="_blank" rel="noreferrer">
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
             </section>
         </div>
     );
